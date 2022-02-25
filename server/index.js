608550ca7233fab36cfaf0835789e0292be00626
Keith Mair <mairck@gmail.com>
2022-02-24 18:39:22 -0700

    added rollbar

--- server/index.js
+++ server/index.js
@@ -3,16 +3,16 @@ const path = require('path')
 
 const app = express()
 
-// // include and initialize the rollbar library with your access token
-// const Rollbar = require('rollbar')
-// const rollbar = new Rollbar({
-//   accessToken: 'ae267a317cd64a53a5cb0c41f4dae18c',
-//   captureUncaught: true,
-//   captureUnhandledRejections: true,
-// })
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: 'd2fe9cbfb62f4bb0af1280d5202e7b89',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
-// // record a generic message and send it to Rollbar
-// rollbar.log('Hello world!')
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
